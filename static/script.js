cca3da74b79ef1e5442c9dd9d71686c28718c1d8
Anna Luiza <annaluizax67@gmail.com>
2025-06-27 13:01:10 -0300

    Finalizing the website style + organizing the code

--- static/script.js
+++ static/script.js
@@ -1,38 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const check = document.getElementById('anonimo');
-    const dados = document.getElementById('identificacao');
-    const inputs = dados.querySelectorAll('input');
+document.addEventListener("DOMContentLoaded", function () {
+  const check = document.getElementById("anonimo");
+  const dados = document.getElementById("identificacao");
+  const inputs = dados.querySelectorAll("input");
 
-    const cepInput = document.querySelector('input[name="cep"]');
-    cepInput.addEventListener('blur', buscarEnderecoPorCep);
+  const cepInput = document.querySelector('input[name="cep"]');
+  cepInput.addEventListener("blur", buscarEnderecoPorCep);
 
-    check.addEventListener('change', function () {
-        if (check.checked) {
-            dados.style.display = 'none';
-            inputs.forEach(input => input.required = false);
-        } else {
-            dados.style.display = 'block';
-            inputs.forEach(input => input.required = true);
-        }
-    });
+  check.addEventListener("change", function () {
+    if (check.checked) {
+      dados.style.display = "none";
+      inputs.forEach((input) => (input.required = false));
+    } else {
+      dados.style.display = "block";
+      inputs.forEach((input) => (input.required = true));
+    }
+  });
 
-    function buscarEnderecoPorCep() {
-    const cep = document.querySelector('input[name="cep"]').value.replace(/\D/g, '');
+  function buscarEnderecoPorCep() {
+    const cep = document
+      .querySelector('input[name="cep"]')
+      .value.replace(/\D/g, "");
 
     if (cep.length !== 8) {
-        return;
+      return;
     }
 
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => response.json())
-        .then(data => {
-            if (!data.erro) {
-                document.querySelector('input[name="logradouro"]').value = data.logradouro || '';
-                document.querySelector('input[name="bairro"]').value = data.bairro || '';
-                document.querySelector('input[name="cidade"]').value = data.localidade || '';
-                document.querySelector('select[name="uf"]').value = data.uf || '';
-            }
-        });
-}
-
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.erro) {
+          document.querySelector('input[name="logradouro"]').value =
+            data.logradouro || "";
+          document.querySelector('input[name="bairro"]').value =
+            data.bairro || "";
+          document.querySelector('input[name="cidade"]').value =
+            data.localidade || "";
+          document.querySelector('select[name="uf"]').value = data.uf || "";
+        }
+      });
+  }
 });
